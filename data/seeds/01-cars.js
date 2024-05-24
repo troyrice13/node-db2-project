d5cdc62b23d455d9c583b56a7745cb1793a2a88e
Troy Rice <troyanthonyrice@gmail.com>
2024-05-24 14:00:12 -0700

    created seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,26 @@
 // STRETCH
+const cars = [
+    {
+        vin: '0000000001',
+        make: 'hyundai',
+        model: 'elantra',
+        mileage: 115000,
+        title: 'clean',
+        transmission: 'auto'
+    },
+    {
+        vin: '0000000002',
+        make: 'toyota',
+        model: 'camry',
+        mileage: 224000,
+        title: 'clean'
+    },
+    {
+        vin: '0000000003',
+        make: 'mercedes-benz',
+        model: 'g-wagon',
+        mileage: 100
+    }
+]
+
+exports.seed = function(knex)
